40d04ec5e046494f5691ca6b8e6b0508021396f7
renaesop <njurjs@gmail.com>
2016-03-01 09:35:21 +0800

    增加自动引入controller的函数

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/3/1.
+ */
+
+const libFolder = './lib/';
+const exportController = require(libFolder + 'export-controller');
+
+exports.exportController = exportController;
